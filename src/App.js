18287566155625c314199b16f2c92469ea94d54a
wbkch <waleedbinkhalidch@gmail.com>
2022-05-09 12:08:05 +0500

    header basis completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import { Fragment } from 'react';
 import './App.css';
+import Header from './Component/Layout/Header';
+import Meals from './Component/Meals/Meals';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </Fragment>
+    
   );
 }
 
